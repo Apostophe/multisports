bf04363c9a624646695894f22f322d80547ac875
Apostophe <christophe.dsneto@gmail.com>
2022-01-09 17:11:58 +0100

    first push with functionnal values and wss connection (creation of matches only)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MatchDisplayComponent } from './match-display/match-display.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'match', component: MatchDisplayComponent },
+  { path: 'options', component: AppComponent },
+  { path: '**', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { webSocket } from "rxjs/webSocket";
+
+import {v4 as uuidv4} from 'uuid';
+
+const refereeConnection = 'ws://warm-dusk-64603.herokuapp.com/referee?refereeID=';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
+  setNumber:number=0;
+
+  points:number=0;
   title = 'multisports';
+
+  increment(value) {
+    if(value == "setNumber")
+      this.setNumber++;
+    else
+      this.points++;
+  };
+  decrement (value) {
+    if(value == "setNumber")
+      this.setNumber--;
+    else
+      this.points--;
+  };
+
+  onSubmit(){
+    let myuuid = uuidv4();
+    console.log('Your UUID is: ' + myuuid);
+    let myWebSocket = webSocket(refereeConnection+myuuid);
+    myWebSocket.subscribe();
+    console.log(myWebSocket);
+    myWebSocket.next({
+      Referee : myuuid,
+      Event : 'match created'
+    });
+  }
 }
