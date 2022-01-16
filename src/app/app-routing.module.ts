d19e47e6aa7a31b381ea21a2a813434eb740291b
Apostophe <christophe.dsneto@gmail.com>
2022-01-16 17:55:44 +0100

    Added design to website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { MatchDisplayComponent } from './match-display/match-display.component';
 
 
 const routes: Routes = [
   { path: 'match', component: MatchDisplayComponent },
-  { path: 'options', component: AppComponent },
-  { path: '**', component: AppComponent}
+  { path: '', component: HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatchDisplayComponent } from './match-display/match-display.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatchDisplayComponent
+    MatchDisplayComponent,
+    HomeComponent    
   ],
   imports: [
     BrowserModule,
