16dbb512019ba3c961a74d994706562edcdeb719
Apostophe <christophe.dsneto@gmail.com>
2022-01-23 16:20:04 +0100

    Commit 2nd part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { MatchDisplayComponent } from './match-display/match-display.component';
+import { RegisterComponent } from './register/register.component';
+import { TournamentCreationComponent } from './tournament-creation/tournament-creation.component';
+import { TournamentDisplayComponent } from './tournament-display/tournament-display.component';
 
 
 const routes: Routes = [
   { path: 'match', component: MatchDisplayComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'creation-tournament', component: TournamentCreationComponent },
+  { path: 'display-tournament', component: TournamentDisplayComponent },
   { path: '', component: HomeComponent},
 ];
 
