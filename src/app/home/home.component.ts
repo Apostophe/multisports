bfb3c3cc6189d52158bd12900dfecb0e4995d6e1
Apostophe <christophe.dsneto@gmail.com>
2022-01-23 16:19:48 +0100

    Commited link to all pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 
 
