c9f4395a1214a757e43b487800f26831e295ded3
Apostophe <christophe.dsneto@gmail.com>
2022-02-25 12:03:35 +0100

    push fix

--- src/app/match-display/match-display.component.ts
+++ src/app/match-display/match-display.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
-
 @Component({
   selector: 'app-match-display',
   templateUrl: './match-display.component.html',
   styleUrls: ['./match-display.component.scss']
 })
 export class MatchDisplayComponent implements OnInit {
-
-
   
   equipeA;
   equipeB;
@@ -17,7 +15,8 @@ export class MatchDisplayComponent implements OnInit {
   nbSetB=0;
   pointA=0;
   pointB=0;
-  constructor(private router: Router) { }
+
+  constructor() { }
   myWebSocketreferee
 
   ngOnInit(): void {
@@ -25,6 +24,7 @@ export class MatchDisplayComponent implements OnInit {
     this.myWebSocketreferee = webSocket(refereeConnection);
     this.myWebSocketreferee.subscribe();
     console.log(this.myWebSocketreferee);
+
 
     let specConnection = 'ws://127.0.0.1:8000/spectateur';
     let myWebSocketspec = webSocket(refereeConnection);
@@ -34,46 +34,51 @@ export class MatchDisplayComponent implements OnInit {
 
   increment(value) {
     if(value == "A"){
+      if (this.pointA>=21 && this.pointA-this.pointB<2){
+        this.pointA++;
+      }
+      if (this.pointA<21){
+        this.pointA++;
+      }
       if(this.pointA==21 && this.pointB<=19){
         this.nbSetA++;
         this.pointA=0;
-      }
-      else if (this.pointA>=21 && this.pointA-this.pointB<2){
-        this.pointA++;
+        this.pointB=0;
       }
       else if (this.pointA>=21 && this.pointA-this.pointB==2){
         this.nbSetA++;
         this.pointA=0;
-      }
-      else{
-        this.pointA++;
+        this.pointB=0;
       }
     }
     if(value == "B"){
+      if (this.pointB>=21 && this.pointB-this.pointA<2){
+        this.pointB++;
+      }
+      if (this.pointB<21){
+        this.pointB++;
+      }
       if(this.pointB==21 && this.pointA<=19){
         this.nbSetB++;
-      }
-      else if (this.pointB>=21 && this.pointB-this.pointA<2){
-        this.pointB++;
+        this.pointA=0;
+        this.pointB=0;
       }
       else if (this.pointB>=21 && this.pointB-this.pointA==2){
         this.nbSetB++;
+        this.pointA=0;
         this.pointB=0;
-      }
-      else{
-        this.pointB++;
       }
     }
     if(this.nbSetA ==2 || this.nbSetB == 2){
-      this.myWebSocketreferee.subscribe()
-      this.router.navigateByUrl("/tournament-home");
+      this.myWebSocketreferee.subscribe();
     }
   };
+
   decrement (value) {
-    if(value ="A" && this.pointA!=0){
+    if(value =="A" && this.pointA!=0){
       this.pointA--
     }
-    else if (this.pointB !=0){
+    else if (value =='B' &&this.pointB !=0){
       this.pointB--;
     }
   };
